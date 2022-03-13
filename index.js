969b33afffedde99fff3d663dc78070e3e652cd2
David De la Hoz <dricardelo@gmail.com>
2022-03-13 18:13:13 -0500

    client controller

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const server = require("./src/app");
 const { conn } = require("./src/db");
 const fillDataBase = require("./src/controllers/data");
 
-conn.sync({ force: true }).then(() => {
-  server.listen(3001, async () => {
-    await fillDataBase();
+conn.sync(/* { force: true } */).then(() => {
+  server.listen(3001, () => {
+    fillDataBase();
     console.log("listening on port 3001");
   });
 });
