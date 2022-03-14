6800fd0ee6e6b3eb489ae9802938ab3b4f38cc71
David De la Hoz <dricardelo@gmail.com>
2022-03-14 18:59:04 -0500

    deleting console.logs

--- src/controllers/clientControllers.js
+++ src/controllers/clientControllers.js
@@ -1,6 +1,4 @@
-const res = require("express/lib/response");
-const { where } = require("sequelize/types");
-const { Client } = require("../db");
+const { Client, Invoice, Product } = require("../db");
 
 const getAllClientsInvoices = async (req, res) => {
   try {
@@ -13,14 +11,41 @@ const getAllClientsInvoices = async (req, res) => {
   }
 };
 
-const getClientById = async (clientId) => {
+const getClientById = async (req, res) => {
+  const { id } = req.params;
   try {
     const client = await Client.findAll({
       where: {
-        id: clientId,
+        id: id,
       },
     });
     console.log(client);
+    res.send(client);
+  } catch (err) {
+    console.log(err);
+    res.send({ error: "error in getClientsById" });
+  }
+};
+
+const getClientsInvoices = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const client = await Client.findAll({
+      where: {
+        id: id,
+      },
+    });
+
+    const allInvoices = await Invoice.findAll({
+      where: {
+        clientId: id,
+      },
+      include: [Client, Product],
+    });
+    res
+      .status(200)
+      .send(await allInvoices.map((invoice) => invoice.dataValues));
   } catch (err) {
     console.log(err);
     res.send({ error: "error in getClientsById" });
@@ -30,4 +55,5 @@ const getClientById = async (clientId) => {
 module.exports = {
   getAllClientsInvoices,
   getClientById,
+  getClientsInvoices,
 };
--- src/routes/client.js
+++ src/routes/client.js
@@ -3,5 +3,6 @@ const clientControllers = require("../controllers/clientControllers");
 
 clientRouter.get("/", clientControllers.getAllClientsInvoices);
 clientRouter.get("/:id", clientControllers.getClientById);
+clientRouter.get("/clientDetails/:id", clientControllers.getClientsInvoices);
 
 module.exports = clientRouter;
